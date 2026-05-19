77ec8771f7739759e4fb3633a31130c508e14500
Marc <spurdo.dk@gmail.com>
2026-05-19 10:48:22 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,5 +15,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`test Server running on http://localhost:${PORT}`);
 });
