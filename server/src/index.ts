835395de8892a18135e7256649a68ac905a662b9
Nina <nsz@heyday.dk>
2026-05-19 09:52:33 +0200

    added swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Swagger docs
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from Express + TS" });
