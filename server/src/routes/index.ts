07ced3d48a34a87fd4f024a968d8e3eefa0c8f08
Marc <spurdo.dk@gmail.com>
2026-05-19 15:34:49 +0200

    Test Done - Survey
    Creating a response row and also make user with status: pending_approval without a password

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import authRoutes from "./auth.routes";
+import surveyRoutes from "./survey.routes";
 
 const router: Router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/survey", surveyRoutes);
 
 export default router;
