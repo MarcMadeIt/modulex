529b799d5fc45dba8ea30e9eeecd5e5f185c3771
Annemette M <annemettemygind@gmail.com>
2026-05-20 08:40:07 +0200

    Moved header to its own comp: layout/Header.vue git commit -m Moved header to its own comp: layout/Header.vue

---
+++ client/src/components/layout/Header.vue
@@ -0,0 +1,37 @@
+//TO DO: Hent brugerdata og hvis i headeren
+
+<template>
+  <header class="dashboard-topbar">
+    <AppButton variant="light" @click="$emit('switch-mode')">
+      Skift til admin
+    </AppButton>
+
+    <div class="notification-button">🔔</div>
+
+    <div class="user-role">
+      <div class="user-icon">👤</div>
+
+      <div class="role-text">
+        {{ userName }}
+        <span>{{ userRole }}</span>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import AppButton from "../ui/AppButton.vue";
+
+defineProps({
+  userName: {
+    type: String,
+    default: "Laura P.",
+  },
+  userRole: {
+    type: String,
+    default: "Partner VP",
+  },
+});
+
+defineEmits(["switch-mode"]);
+</script>
